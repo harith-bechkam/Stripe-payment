25cb9ac53101b74abd8f729893cc283f0d27fdfc
harith <harith.kv47@gmail.com>
2022-04-12 14:34:52 +0530

    fghfxtetsd

--- src/helpers/constant.js
+++ src/helpers/constant.js
@@ -1,3 +1,3 @@
-export const END_POINT = 'http://localhost:5000';
+export const END_POINT = 'https://powerful-wildwood-75399.herokuapp.com/';
 //http://localhost:5000
 //https://payment-gateway-node-be.herokuapp.com
